964ca2e209e1f614c1af5403a728bac4aac72c66
Juan Sebastian <sebassossa02@gmail.com>
2025-07-21 02:02:39 -0500

    ✨ New icon mpper to manegement icons

---
+++ src/components/commercial/hooks/mappers/iconMapper.js
@@ -0,0 +1,13 @@
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import PercentIcon from '@mui/icons-material/Percent';
+
+const iconMap = {
+  MonetizationOnIcon: MonetizationOnIcon,
+  StorefrontIcon: StorefrontIcon,
+  ShoppingCartIcon: ShoppingCartIcon,
+  PercentIcon: PercentIcon,
+};
+
+export default iconMap;
